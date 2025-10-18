630fab1813cd4c5ceda379cdd0fdef11fa251413
prakashsharma720 <34154254+prakashsharma720@users.noreply.github.com>
2025-10-18 10:43:07 +0530

    dfg

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php 
-$base_url = 'http://localhost/s4-smart-shop/';
+$base_url = 'http://localhost/ci/s4-smart-shop/';
 
 $conn = new mysqli("localhost", "root", "", "s4shopdb");
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
